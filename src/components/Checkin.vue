d110eb4903701a9de3674284425e1b0b8bc6c1e2
chinwat kotchum <chinwat@odd.works>
2019-09-13 16:08:37 +0700

    init scrolla

--- src/components/Checkin.vue
+++ src/components/Checkin.vue
@@ -1,15 +1,54 @@
 <template>
-    <div>
-        <h2>checkin route</h2>
+  <div>
+    <h2>checkin route</h2>
+    <div class="timer">
+      <smooth-picker v-show="showPicker" ref="smoothPicker" :data="data" />
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'Checkin'
+  name: 'Checkin',
+  data () {
+    let hours = []
+    let minutes = []
+    for (let h = 0; h < 24; h++) {
+      hours.push(h)
+    }
+    for (let m = 0; m < 60; m++) {
+      minutes.push(m)
+    }
+    return {
+      showPicker: true,
+      data: [
+        {
+          currentIndex: 1,
+          flex: 3,
+          list: hours,
+          onClick: this.clickOnGender,
+          textAlign: 'center',
+          className: 'row-group'
+        },
+        {
+          currentIndex: 1,
+          flex: 3,
+          list: minutes,
+          onClick: this.clickOnGender,
+          textAlign: 'center',
+          className: 'row-group'
+        }
+      ]
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.timer {
+  margin-left: auto;
+  margin-right: auto;
+  width: 300px;
+  border: solid blue 2px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'vue-smooth-picker/dist/css/style.css'
+import SmoothPicker from 'vue-smooth-picker'
 
 Vue.config.productionTip = false
+Vue.use(SmoothPicker)
 
 /* eslint-disable no-new */
 new Vue({
